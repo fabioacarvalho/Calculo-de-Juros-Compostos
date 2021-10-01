2325829aac5e037c5760c5ea1269610683370fbe
Fábio Carvalho <fabioxcarvalho@outlook.com>
2021-10-01 16:17:56 -0300

    update

---
+++ jurosc/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        
+        <span>Calculo de Juros Compostos</span>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      
+    </v-app-bar>
+
+    <v-main>
+      <Body/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Body from './components/Body';
+
+export default {
+  name: 'App',
+
+  components: {
+    Body,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ jurosc/src/components/Body.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container fluid>
+      <div class="ma-16">
+        <v-text-field
+          label="Main input"
+          :rules="rules"
+          hide-details="auto"
+        ></v-text-field>
+        <v-text-field label="Another input"></v-text-field>
+      </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+      ],
+    }),
+  }
+</script>
+
+<style >
+    
+
+</style>
